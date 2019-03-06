51a980021070afffa67f753a70e967a53e4f6f96
Allen <Allink28@gmail.com>
2019-03-05 21:57:50 -0500

    Add edge detection

--- pygta5-1.py
+++ pygta5-1.py
@@ -4,21 +4,30 @@ import cv2
 import time
 
 
+def process_img(original_image):
+    """
+    Edge detection!
+    :return: A grayscale image with edges detected
+    """
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    return processed_img
+
+
 def screen_record():
     last_time = time.time()
     while True:
         # 800x600 windowed mode
-        printscreen = np.array(ImageGrab.grab(bbox=(0,40, 800,640)))
+        screen = np.array(ImageGrab.grab(bbox=(0,40, 800,640)))
         print('loop took {} seconds'.format(time.time()-last_time))
         last_time = time.time()
-        cv2.imshow('window', cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        new_screen = process_img(screen)
+        cv2.imshow('window', new_screen)
+        # cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
 
-
-# def main():
-#     screen_record()
 
 if __name__ == "__main__":
     screen_record()
